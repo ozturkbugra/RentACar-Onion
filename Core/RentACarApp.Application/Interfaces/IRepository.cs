b2eefd718f4b2d4bb7bf9a2f70b52d2a86205f1b
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-01-31 00:12:57 +0300

    Repository interface'si oluşturuldu.

---
+++ Core/RentACarApp.Application/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RentACarApp.Application.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<List<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task CreateAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task RemoveAsync(int id);
+    }
+}
