9c3a37378017b827f7580159f4ca4bcc453dd320
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-02 00:44:15 +0300

    Ana sayfaya hizmetlerimiz çekildi ve viewcomponent oluşturuldu.

---
+++ Frontends/RentACarApp.WebUI/ViewComponents/ServiceComponents/_ServiceComponentPartial.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RentACarApp.Dto.ServiceDtos;
+
+namespace RentACarApp.WebUI.ViewComponents.ServiceComponents
+{
+    public class _ServiceComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _ServiceComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7066/api/Services");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = System.Text.Json.JsonSerializer.Deserialize<List<ResultServiceDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
