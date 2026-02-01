ef0d4d15d80ce8def9504c12b7d9bb56e7e33aa8
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-01 23:29:59 +0300

    Referanslar About sayfasında listelendi.

---
+++ Frontends/RentACarApp.WebUI/ViewComponents/AboutViewComponents/_TestimonialComponentPartial.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RentACarApp.Dto.AboutDtos;
+using RentACarApp.Dto.TestimonialDtos;
+using System.Threading.Tasks;
+
+namespace RentACarApp.WebUI.ViewComponents.AboutViewComponents
+{
+    public class _TestimonialComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _TestimonialComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7066/api/Testimonials");
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = System.Text.Json.JsonSerializer.Deserialize<List<ResultTestimonialDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
