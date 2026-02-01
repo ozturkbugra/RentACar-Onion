8acceb0c164b5c4566f8b0ee6a0f8a38fbac3502
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-02 00:05:13 +0300

    Contact sayfası yapıldı post işlemi ile apiye create işlemi yaptırıldı.

---
+++ Frontends/RentACarApp.Dto/ContactDtos/CreateContactDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarApp.Dto.ContactDtos
+{
+    public class CreateContactDto
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public string subject { get; set; }
+        public string message { get; set; }
+        public DateTime sendDate { get; set; }
+    }
+}
--- Frontends/RentACarApp.WebUI/Controllers/ContactController.cs
+++ Frontends/RentACarApp.WebUI/Controllers/ContactController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentACarApp.Dto.ContactDtos;
+using System.Text;
 
 namespace RentACarApp.WebUI.Controllers
 {
@@ -13,6 +16,21 @@ namespace RentACarApp.WebUI.Controllers
 
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateContactDto createContactDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            createContactDto.sendDate = DateTime.Now;
+            var jsondata = JsonConvert.SerializeObject(createContactDto);
+            StringContent stringContent = new StringContent(jsondata, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync("https://localhost:7066/api/Contacts", stringContent);
+            if(response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "About");
+            }
             return View();
         }
     }
