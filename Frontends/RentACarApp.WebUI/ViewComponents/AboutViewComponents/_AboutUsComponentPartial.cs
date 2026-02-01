fa75e6a0337c0c597e7109ae633cddba53a38d00
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-01 19:39:23 +0300

    about sayfası hazırlandı.

---
+++ Frontends/RentACarApp.WebUI/ViewComponents/AboutViewComponents/_AboutUsComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RentACarApp.WebUI.ViewComponents.AboutViewComponents
+{
+    public class _AboutUsComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
