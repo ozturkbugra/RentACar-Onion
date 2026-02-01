11fb1506da5100abab172163fd51b32d8f85b867
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-01 15:20:59 +0300

    Pricing API ve Mapping işlemleri yapıldı.

---
+++ Core/RentACarApp.Application/Features/Mappings/PricingMapping.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using RentACarApp.Application.Features.Mediator.Commands.PricingCommands;
+using RentACarApp.Application.Features.Mediator.Results.PricingResults;
+using RentACarApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarApp.Application.Features.Mappings
+{
+    public class PricingMapping:Profile
+    {
+        public PricingMapping()
+        {
+            CreateMap<Pricing, GetPricingQueryResult>();
+            CreateMap<Pricing, GetPricingByIdQueryResult>();
+            CreateMap<CreatePricingCommand, Pricing>();
+            CreateMap<UpdatePricingCommand, Pricing>();
+        }
+    }
+}
---
+++ Presentation/RentACarApp.WebApi/Controllers/PricingsController.cs
@@ -0,0 +1,54 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RentACarApp.Application.Features.Mediator.Commands.PricingCommands;
+using RentACarApp.Application.Features.Mediator.Queries.PricingQueries;
+
+namespace RentACarApp.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PricingsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public PricingsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> PricingList()
+        {
+            var result = await _mediator.Send(new GetPricingQuery());
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetPricingById(int id)
+        {
+            var result = await _mediator.Send(new GetPricingByIdQuery(id));
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreatePricing(CreatePricingCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Pricing eklendi");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdatePricing(UpdatePricingCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Pricing güncellendi");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePricing(int id)
+        {
+            await _mediator.Send(new RemovePricingCommand(id));
+            return Ok("Pricing silindi");
+        }
+    }
+}
