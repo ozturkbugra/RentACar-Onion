b1cd222153873491c8f1a5a7f6c134c607004fcf
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-01 23:56:47 +0300

    Contact sayfasına iletişim bilgileri çekildi.

---
+++ Frontends/RentACarApp.Dto/FooterAddressDtos/ResultFooterAddressDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarApp.Dto.FooterAddressDtos
+{
+    public class ResultFooterAddressDto
+    {
+        public int footerAddressID { get; set; }
+
+        public string description { get; set; }
+
+        public string address { get; set; }
+
+        public string phone { get; set; }
+
+        public string email { get; set; }
+    }
+}
---
+++ Frontends/RentACarApp.WebUI/ViewComponents/FooterAddressComponents/_FooterAddressComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RentACarApp.Dto.FooterAddressDtos;
+using RentACarApp.Dto.TestimonialDtos;
+
+namespace RentACarApp.WebUI.ViewComponents.FooterAddressComponents
+{
+    public class _FooterAddressComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _FooterAddressComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7066/api/FooterAddresses");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = System.Text.Json.JsonSerializer.Deserialize<List<ResultFooterAddressDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
