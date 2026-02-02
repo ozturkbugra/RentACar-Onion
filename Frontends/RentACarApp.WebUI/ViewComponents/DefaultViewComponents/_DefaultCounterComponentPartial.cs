493acf7a898d639c267c70f23b243628cd08da4a
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-02 20:35:47 +0300

    Counter ViewComponente çekildi ve Default page sadeleştirilmeye devam edildi.

---
+++ Frontends/RentACarApp.WebUI/ViewComponents/DefaultViewComponents/_DefaultCounterComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RentACarApp.WebUI.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultCounterComponentPartial : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        } 
+    }
+}
