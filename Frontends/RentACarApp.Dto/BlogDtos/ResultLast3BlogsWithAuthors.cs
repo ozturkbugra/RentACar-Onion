bbb70d689091e45e14acd4751a2da3f9c5fe0674
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-02 20:29:04 +0300

    Son 3 blog ana sayfada listelendi.

---
+++ Frontends/RentACarApp.Dto/BlogDtos/ResultLast3BlogsWithAuthors.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarApp.Dto.BlogDtos
+{
+    public class ResultLast3BlogsWithAuthors
+    {
+        public int blogID { get; set; }
+        public string title { get; set; }
+        public int authorID { get; set; }
+        public string authorName { get; set; }
+        public string coverImageUrl { get; set; }
+        public DateTime createdDate { get; set; }
+        public int categoryID { get; set; }
+    }
+}
---
+++ Frontends/RentACarApp.WebUI/ViewComponents/BlogViewComponents/_GetLast3BlogsWithAuthorsListComponentPartial.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RentACarApp.Dto.AboutDtos;
+using RentACarApp.Dto.BlogDtos;
+
+namespace RentACarApp.WebUI.ViewComponents.BlogViewComponents
+{
+    public class _GetLast3BlogsWithAuthorsListComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _GetLast3BlogsWithAuthorsListComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7066/api/Blogs/GetLast3BlogsWithAuthors");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = System.Text.Json.JsonSerializer.Deserialize<List<ResultLast3BlogsWithAuthors>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+    }
+}
