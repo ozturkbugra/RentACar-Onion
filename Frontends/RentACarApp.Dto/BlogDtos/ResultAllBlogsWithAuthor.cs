e9ea6339f73906323914b8910b0c1ef52d26245e
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-03 20:01:16 +0300

    Bloglar blog sayfasında listelendi.

---
+++ Frontends/RentACarApp.Dto/BlogDtos/ResultAllBlogsWithAuthor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarApp.Dto.BlogDtos
+{
+    public class ResultAllBlogsWithAuthor
+    {
+        public int blogID { get; set; }
+        public string title { get; set; }
+        public int authorID { get; set; }
+        public string authorName { get; set; }
+        public string categoryName { get; set; }
+        public string coverImageUrl { get; set; }
+        public DateTime createdDate { get; set; }
+        public int categoryID { get; set; }
+    }
+}
---
+++ Frontends/RentACarApp.WebUI/Controllers/BlogController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentACarApp.Dto.BlogDtos;
+using System.Threading.Tasks;
+
+namespace RentACarApp.WebUI.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BlogController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7066/api/Blogs/GetAllBlogsWithAuthor");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData =  await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAllBlogsWithAuthor>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
