ae9a746b49565d6043c06edbe178d8464c777969
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-03 20:49:44 +0300

    Blog detay sayfasına kategoriler listelendi.

---
+++ Frontends/RentACarApp.Dto/CategoryDtos/ResultCategoryDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarApp.Dto.CategoryDtos
+{
+    public class ResultCategoryDto
+    {
+        public int categoryId { get; set; }
+        public string name { get; set; }
+    }
+}
--- Frontends/RentACarApp.WebUI/ViewComponents/BlogViewComponents/_BlogDetailsCategoryViewComponentPartial.cs
+++ Frontends/RentACarApp.WebUI/ViewComponents/BlogViewComponents/_BlogDetailsCategoryViewComponentPartial.cs
@@ -1,12 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RentACarApp.Dto.CategoryDtos;
+using RentACarApp.Dto.ServiceDtos;
 
 namespace RentACarApp.WebUI.ViewComponents.BlogViewComponents
 {
     public class _BlogDetailsCategoryViewComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _BlogDetailsCategoryViewComponentPartial(IHttpClientFactory httpClientFactory)
         {
-            return View(); 
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7066/api/Categories");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = System.Text.Json.JsonSerializer.Deserialize<List<ResultCategoryDto>>(jsonData);
+                return View(values);
+            }
+            return View();
         }
     }
 }
