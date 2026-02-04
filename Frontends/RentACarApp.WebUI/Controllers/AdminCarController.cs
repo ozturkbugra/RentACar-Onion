3b3c78163ddde0d1e786a12c2774660f8b3b13b1
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-04 22:04:33 +0300

    Dropdown'a araba markaları getirildi.

--- Frontends/RentACarApp.WebUI/Controllers/AdminCarController.cs
+++ Frontends/RentACarApp.WebUI/Controllers/AdminCarController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
+using RentACarApp.Dto.BrandDtos;
 using RentACarApp.Dto.CarDtos;
 
 namespace RentACarApp.WebUI.Controllers
@@ -27,6 +29,20 @@ namespace RentACarApp.WebUI.Controllers
         }
         public async Task<IActionResult> CreateCar()
         {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7066/api/Brands");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBrandDto>>(jsonData);
+                List<SelectListItem> brandValues = (from x in values
+                                                    select new SelectListItem
+                                                    {
+                                                        Text = x.name,
+                                                        Value = x.brandID.ToString()
+                                                    }).ToList();
+                ViewBag.BrandValues = brandValues;
+            }
             return View();
         }
     }
