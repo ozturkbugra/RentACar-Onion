3371064bc166817da073351c67264b8991ce9c98
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-04 23:01:01 +0300

    Özellik silme ve güncelleme işlemleri yapıldı.

--- Frontends/RentACarApp.WebUI/Controllers/AdminFeature.cs
+++ Frontends/RentACarApp.WebUI/Controllers/AdminFeature.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
+using RentACarApp.Dto.BrandDtos;
 using RentACarApp.Dto.CarDtos;
 using RentACarApp.Dto.FeatureDtos;
 using System.Text;
@@ -45,6 +47,42 @@ namespace RentACarApp.WebUI.Controllers
                 return RedirectToAction("Index");
             }
             return View();
+        }
+
+        public async Task<IActionResult> RemoveFeature(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync("https://localhost:7066/api/Features/" + id);
+            return RedirectToAction("Index");
+
+        }
+
+        public async Task<IActionResult> UpdateFeature(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            var responseMessage = await client.GetAsync("https://localhost:7066/api/Features/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ResultFeatureDto>(jsonData);
+                return View(values);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateFeature(ResultFeatureDto resultFeatureDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var JsonData = JsonConvert.SerializeObject(resultFeatureDto);
+            StringContent stringContent = new StringContent(JsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7066/api/Features", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(resultFeatureDto);
         }
     }
 }
