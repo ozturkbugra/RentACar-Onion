e1793f0b61ff55215f90ceaa3e0a1cb22854c664
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-04 21:53:41 +0300

    Araç listeleme yapıldı ve araç ekleme işlemine geçildi.

--- Frontends/RentACarApp.WebUI/Controllers/AdminCarController.cs
+++ Frontends/RentACarApp.WebUI/Controllers/AdminCarController.cs
@@ -1,10 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentACarApp.Dto.CarDtos;
 
 namespace RentACarApp.WebUI.Controllers
 {
     public class AdminCarController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminCarController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7066/api/Cars/GetCarWithBrand");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCarWithBrandDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        public async Task<IActionResult> CreateCar()
         {
             return View();
         }
