93d1a946af2f6109a1a86be9e892798e65ed467c
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-04 22:17:23 +0300

    Araba ekleme işlemi yapıldı.

--- Frontends/RentACarApp.WebUI/Controllers/AdminCarController.cs
+++ Frontends/RentACarApp.WebUI/Controllers/AdminCarController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
 using RentACarApp.Dto.BrandDtos;
 using RentACarApp.Dto.CarDtos;
+using System.Text;
 
 namespace RentACarApp.WebUI.Controllers
 {
@@ -45,5 +46,20 @@ namespace RentACarApp.WebUI.Controllers
             }
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCar(CreateCarDto createCarDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createCarDto);
+            StringContent stringContent = new StringContent(jsonData,Encoding.UTF8,"application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7066/api/Cars", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
     }
 }
