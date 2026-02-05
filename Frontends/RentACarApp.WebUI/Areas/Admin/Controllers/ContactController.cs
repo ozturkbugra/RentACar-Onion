28be514aea96762cca81afd4cea1c9ca4a2f858f
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-05 22:17:20 +0300

    Mesajlar listelenme ve silme işlemleri yapıldı.

---
+++ Frontends/RentACarApp.WebUI/Areas/Admin/Controllers/ContactController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentACarApp.Dto.ContactDtos;
+using RentACarApp.Dto.BrandDtos;
+using System.Text;
+
+namespace RentACarApp.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("/Admin/Contact")]
+    public class ContactController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ContactController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("Index")]
+        [Route("")]
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7066/api/Contacts");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultContactDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [Route("Remove/{id}")]
+        public async Task<IActionResult> Remove(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync("https://localhost:7066/api/Contacts/" + id);
+            return RedirectToAction("Index", "Contact", new { area = "Admin" });
+
+        }
+
+       
+    }
+}
