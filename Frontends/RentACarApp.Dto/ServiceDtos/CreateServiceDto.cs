ded97b09281c22274cf52409819af7677fd07d41
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-05 22:35:37 +0300

    Service CRUD işlemleri yapıldı.

---
+++ Frontends/RentACarApp.Dto/ServiceDtos/CreateServiceDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarApp.Dto.ServiceDtos
+{
+    public class CreateServiceDto
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+        public string iconUrl { get; set; }
+    }
+}
