e73e72629ff579977a860f61bd7ea03e26f0437a
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-05 21:19:48 +0300

    Author CRUD işlemleri yapıldı.

---
+++ Frontends/RentACarApp.WebUI/Areas/Admin/Controllers/AuthorController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentACarApp.Dto.AuthorDtos;
+using System.Text;
+
+namespace RentACarApp.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("/Admin/Author")]
+    public class AuthorController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AuthorController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("Index")]
+        [Route("")]
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7066/api/Authors");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAuthorDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [Route("Create")]
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        public async Task<IActionResult> Create(CreateAuthorDto createAuthorDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createAuthorDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7066/api/Authors", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Author", new { area = "Admin" });
+            }
+            return View();
+        }
+
+        [Route("Remove/{id}")]
+        public async Task<IActionResult> Remove(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync("https://localhost:7066/api/Authors/" + id);
+            return RedirectToAction("Index", "Author", new { area = "Admin" });
+
+        }
+
+        [Route("Update/{id}")]
+        public async Task<IActionResult> Update(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            var responseMessage = await client.GetAsync("https://localhost:7066/api/Authors/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ResultAuthorDto>(jsonData);
+                return View(values);
+            }
+            return RedirectToAction("Index", "Author", new { area = "Admin" });
+        }
+
+        [HttpPost]
+        [Route("Update/{id}")]
+        public async Task<IActionResult> Update(ResultAuthorDto resultAuthorDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var JsonData = JsonConvert.SerializeObject(resultAuthorDto);
+            StringContent stringContent = new StringContent(JsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7066/api/Authors", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Author", new { area = "Admin" });
+            }
+            return View(resultAuthorDto);
+        }
+    }
+}
