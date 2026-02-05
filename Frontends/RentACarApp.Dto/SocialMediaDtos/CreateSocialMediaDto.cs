473acfffbf0f5c7e0b2bdad91ab8583f4fd36ad6
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-05 22:40:52 +0300

    SocialMedia CRUD işlemleri yapıldı.

---
+++ Frontends/RentACarApp.Dto/SocialMediaDtos/CreateSocialMediaDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarApp.Dto.SocialMediaDtos
+{
+    public class CreateSocialMediaDto
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+        public string icon { get; set; }
+    }
+}
---
+++ Frontends/RentACarApp.Dto/SocialMediaDtos/ResultSocialMediaDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarApp.Dto.SocialMediaDtos
+{
+    public class ResultSocialMediaDto
+    {
+        public int socialMediaID { get; set; }
+        public string name { get; set; }
+        public string url { get; set; }
+        public string icon { get; set; }
+    }
+}
