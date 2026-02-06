a8d9f4db0dc5ee2e287e868564bd8fef592c2428
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-06 18:30:26 +0300

    İstatistik sayfası oluşturuldu.

---
+++ Frontends/RentACarApp.WebUI/Areas/Admin/Controllers/StatisticController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RentACarApp.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("/Admin/Statistic")]
+    public class StatisticController : Controller
+    {
+        [Route("Index")]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
