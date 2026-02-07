6fe55dd93a34e4e5631dcd7f84055d3ea7c1fa36
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-07 17:57:12 +0300

    İstastikler UI'a çekildi.

---
+++ Frontends/RentACarApp.Dto/StatisticDtos/ResultStatisticDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarApp.Dto.StatisticDtos
+{
+    public class ResultStatisticDto
+    {
+        public string MaxCarByBrand { get; set; }
+
+        public int CarCount { get; set; }
+
+        public int BrandCount { get; set; }
+
+        public decimal AvgCarPrice { get; set; }
+    }
+}
--- Frontends/RentACarApp.WebUI/Areas/Admin/Controllers/StatisticController.cs
+++ Frontends/RentACarApp.WebUI/Areas/Admin/Controllers/StatisticController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentACarApp.Dto.AboutDtos;
+using RentACarApp.Dto.StatisticDtos;
 
 namespace RentACarApp.WebUI.Areas.Admin.Controllers
 {
@@ -6,11 +9,56 @@ namespace RentACarApp.WebUI.Areas.Admin.Controllers
     [Route("/Admin/Statistic")]
     public class StatisticController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public StatisticController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
         [Route("Index")]
         [Route("")]
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var client = _httpClientFactory.CreateClient();
+            var model = new ResultStatisticDto();
+
+            var response1 = await client.GetAsync("https://localhost:7066/api/Statistics/GetCarCount");
+            if (response1.IsSuccessStatusCode)
+            {
+                var jsonData = await response1.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<ResultStatisticDto>(jsonData);
+                model.CarCount = value.CarCount;
+            }
+
+            var response2 = await client.GetAsync("https://localhost:7066/api/Statistics/AvgCarPricingDaily");
+            if (response2.IsSuccessStatusCode)
+            {
+                var jsonData = await response2.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<ResultStatisticDto>(jsonData);
+                model.AvgCarPrice = value.AvgCarPrice;
+            }
+
+            var response3 = await client.GetAsync("https://localhost:7066/api/Statistics/MaxCarByBrand");
+            if (response3.IsSuccessStatusCode)
+            {
+                var jsonData = await response3.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<ResultStatisticDto>(jsonData);
+                model.MaxCarByBrand = value.MaxCarByBrand;
+            }
+
+            var response4 = await client.GetAsync("https://localhost:7066/api/Statistics/BrandCount");
+            if (response4.IsSuccessStatusCode)
+            {
+                var jsonData = await response4.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<ResultStatisticDto>(jsonData);
+                model.BrandCount = value.BrandCount;
+            }
+
+            return View(model);
         }
+
+
+
     }
 }
