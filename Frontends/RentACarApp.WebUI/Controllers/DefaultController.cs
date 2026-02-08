76be5667719b0aadd93a8c5475a8223ca9312cea
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-08 14:38:31 +0300

    Ana Sayfada locationlar listelendi.

--- Frontends/RentACarApp.WebUI/Controllers/DefaultController.cs
+++ Frontends/RentACarApp.WebUI/Controllers/DefaultController.cs
@@ -1,11 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+using RentACarApp.Dto.LocationDtos;
 
 namespace RentACarApp.WebUI.Controllers
 {
     public class DefaultController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public DefaultController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7066/api/Locations");
+
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultLocationDto>>(jsonData);
+            List<SelectListItem> values2 = (from x in values
+                                            select new SelectListItem
+                                            {
+                                                Text = x.name,
+                                                Value = x.locationID.ToString(),
+                                            }).ToList();
+            ViewBag.Locations = values2;
             return View();
         }
     }
