2bb4fec113a0f28316e2d77a4f8f05688c4c8678
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-08 14:11:54 +0300

    RentACar Repository ve Interface'leri oluşturuldu.

---
+++ Infrastructure/RentACarApp.Persistence/Repositories/RentACarRepositories/RentACarRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentACarApp.Application.Interfaces.RentACarInterfaces;
+using RentACarApp.Domain.Entities;
+using RentACarApp.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarApp.Persistence.Repositories.RentACarRepositories
+{
+    public class RentACarRepository : IRentACarRepository
+    {
+        private readonly RentACarAppContext _context;
+
+        public RentACarRepository(RentACarAppContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<RentACar>> GetByFilterAsync(Expression<Func<RentACar, bool>> filter)
+        {
+            var values = await _context.RentACars.Where(filter).ToListAsync();
+            return values;
+        }
+    }
+}
