d4acb8c87ddcc1c25ef9426e2825987c451a625e
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-08 14:18:56 +0300

    RentACar sınfının mediator işlemleri yapıldı.

---
+++ Core/RentACarApp.Application/Features/Mediator/Handlers/RentACarHandlers/GetRentACarQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using RentACarApp.Application.Features.Mediator.Queries.RentACarQueries;
+using RentACarApp.Application.Features.Mediator.Results.RentACarResults;
+using RentACarApp.Application.Interfaces.RentACarInterfaces;
+using RentACarApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarApp.Application.Features.Mediator.Handlers.RentACarHandlers
+{
+    public class GetRentACarQueryHandler : IRequestHandler<GetRentACarQuery, List<GetRentACarQueryResult>>
+    {
+        private readonly IRentACarRepository _repository;
+
+        public GetRentACarQueryHandler(IRentACarRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<GetRentACarQueryResult>> Handle(GetRentACarQuery request, CancellationToken cancellationToken)
+        {
+            var values = await _repository.GetByFilterAsync(x=> x.LocationID == request.LocationID && x.Available == true);
+            return values.Select(x => new GetRentACarQueryResult
+            {
+                CarID = x.CarID,
+            }).ToList();
+        }
+    }
+}
---
+++ Core/RentACarApp.Application/Features/Mediator/Results/RentACarResults/GetRentACarQueryResult.cs
@@ -0,0 +1,14 @@
+﻿using RentACarApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarApp.Application.Features.Mediator.Results.RentACarResults
+{
+    public class GetRentACarQueryResult
+    {
+        public int CarID { get; set; }
+    }
+}
