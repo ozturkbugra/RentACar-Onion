1dc268e3be580a5d650441562465244752c00fdb
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-08 21:43:29 +0300

    Reservation ekleme işleminin mediator'u yazıldı.

---
+++ Core/RentACarApp.Application/Features/Mappings/ReservationMapping.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using RentACarApp.Application.Features.Mediator.Commands.ReservationCommands;
+using RentACarApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarApp.Application.Features.Mappings
+{
+    public class ReservationMapping : Profile
+    {
+        public ReservationMapping()
+        {
+            CreateMap<CreateReservationCommand, Reservation>();
+        }
+    }
+}
---
+++ Core/RentACarApp.Application/Features/Mediator/Handlers/ReservationHandlers/CreateReservationCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using MediatR;
+using RentACarApp.Application.Features.Mediator.Commands.ReservationCommands;
+using RentACarApp.Application.Interfaces;
+using RentACarApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarApp.Application.Features.Mediator.Handlers.ReservationHandlers
+{
+    public class CreateReservationCommandHandler : IRequestHandler<CreateReservationCommand>
+    {
+        private readonly IRepository<Reservation> _repository;
+        private readonly IMapper _mapper;
+
+        public CreateReservationCommandHandler(IRepository<Reservation> repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task Handle(CreateReservationCommand request, CancellationToken cancellationToken)
+        {
+            var reservation = _mapper.Map<Reservation>(request);
+            await _repository.CreateAsync(reservation);
+        }
+    }
+}
