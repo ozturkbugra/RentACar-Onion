25a56a772ed247112b8e26a009dac58c4f61d2ea
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-09 22:52:53 +0300

    Arabaya ait özellikler listelendi.

---
+++ Frontends/RentACarApp.Dto/CarFeatureDtos/ResultListCarFeatureByCarIdDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarApp.Dto.CarFeatureDtos
+{
+    public class ResultListCarFeatureByCarIdDto
+    {
+        public int carFeatureID { get; set; }
+        public int carID { get; set; }
+        public int featureID { get; set; }
+        public string featureName { get; set; }
+        public bool available { get; set; }
+    }
+}
--- Frontends/RentACarApp.WebUI/Areas/Admin/Controllers/CarFeatureController.cs
+++ Frontends/RentACarApp.WebUI/Areas/Admin/Controllers/CarFeatureController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentACarApp.Dto.BlogDtos;
+using RentACarApp.Dto.CarFeatureDtos;
 
 namespace RentACarApp.WebUI.Areas.Admin.Controllers
 {
@@ -6,9 +9,23 @@ namespace RentACarApp.WebUI.Areas.Admin.Controllers
     [Route("/Admin/CarFeature")]
     public class CarFeatureController : Controller
     {
-        [Route("Index/{id}")]
-        public IActionResult Index(int id)
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CarFeatureController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        [Route("Index/{id}")]
+        public async Task<IActionResult> Index(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7066/api/CarFeatures?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultListCarFeatureByCarIdDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
