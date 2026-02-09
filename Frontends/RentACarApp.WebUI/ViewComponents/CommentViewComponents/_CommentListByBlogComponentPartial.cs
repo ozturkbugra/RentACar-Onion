d5f433f87405e8d2993ed3a10d0029db2d23beb1
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-09 20:40:56 +0300

    Blog yorumları ilgili bloglarda listelendi.

--- Frontends/RentACarApp.WebUI/ViewComponents/CommentViewComponents/_CommentListByBlogComponentPartial.cs
+++ Frontends/RentACarApp.WebUI/ViewComponents/CommentViewComponents/_CommentListByBlogComponentPartial.cs
@@ -1,11 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RentACarApp.Dto.CommentDtos;
 
 namespace RentACarApp.WebUI.ViewComponents.CommentViewComponents
 {
     public class _CommentListByBlogComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _CommentListByBlogComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7066/api/Comments/CommentListByBlog/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = System.Text.Json.JsonSerializer.Deserialize<List<ResultCommentDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
