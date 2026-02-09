24b0e1be9950fe445b39cc1048f534f9c8186635
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-09 18:25:39 +0300

    İstatistikleri ana sayfada gösterdik.

--- Frontends/RentACarApp.WebUI/ViewComponents/DefaultViewComponents/_DefaultCounterComponentPartial.cs
+++ Frontends/RentACarApp.WebUI/ViewComponents/DefaultViewComponents/_DefaultCounterComponentPartial.cs
@@ -1,12 +1,43 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentACarApp.Dto.StatisticDtos;
+using System.Net.Http;
 
 namespace RentACarApp.WebUI.ViewComponents.DefaultViewComponents
 {
     public class _DefaultCounterComponentPartial : ViewComponent
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultCounterComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            return View();
+
+            var client = _httpClientFactory.CreateClient();
+            var model = new ResultStatisticDto();
+
+            var response1 = await client.GetAsync("https://localhost:7066/api/Statistics/GetCarCount");
+            if (response1.IsSuccessStatusCode)
+            {
+                var jsonData = await response1.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<ResultStatisticDto>(jsonData);
+                model.CarCount = value.CarCount;
+            }
+
+            var response4 = await client.GetAsync("https://localhost:7066/api/Statistics/BrandCount");
+            if (response4.IsSuccessStatusCode)
+            {
+                var jsonData = await response4.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<ResultStatisticDto>(jsonData);
+                model.BrandCount = value.BrandCount;
+            }
+
+            return View(model);
+
         } 
     }
 }
