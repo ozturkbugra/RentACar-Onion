eceda14d8ef3afc7049e19d878dd89e3f2b90bb4
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-10 22:04:10 +0300

    Dashboardda ilk grafik alanı doldu.

---
+++ Frontends/RentACarApp.WebUI/ViewComponents/DashboardComponents/_AdminDashBoardStatisticComponentPartial.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentACarApp.Dto.StatisticDtos;
+
+namespace RentACarApp.WebUI.ViewComponents.DashboardComponents
+{
+    public class _AdminDashBoardStatisticComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _AdminDashBoardStatisticComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var model = new ResultStatisticDto();
+
+            var response1 = await client.GetAsync("https://localhost:7066/api/Statistics/GetCarCount");
+            if (response1.IsSuccessStatusCode)
+            {
+                var jsonData = await response1.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<ResultStatisticDto>(jsonData);
+                model.CarCount = value.CarCount;
+            }
+
+            var response2 = await client.GetAsync("https://localhost:7066/api/Statistics/AvgCarPricingDaily");
+            if (response2.IsSuccessStatusCode)
+            {
+                var jsonData = await response2.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<ResultStatisticDto>(jsonData);
+                model.AvgCarPrice = value.AvgCarPrice;
+            }
+
+            var response3 = await client.GetAsync("https://localhost:7066/api/Statistics/MaxCarByBrand");
+            if (response3.IsSuccessStatusCode)
+            {
+                var jsonData = await response3.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<ResultStatisticDto>(jsonData);
+                model.MaxCarByBrand = value.MaxCarByBrand;
+            }
+
+            var response4 = await client.GetAsync("https://localhost:7066/api/Statistics/BrandCount");
+            if (response4.IsSuccessStatusCode)
+            {
+                var jsonData = await response4.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<ResultStatisticDto>(jsonData);
+                model.BrandCount = value.BrandCount;
+            }
+
+            return View(model);
+        }
+    }
+}
