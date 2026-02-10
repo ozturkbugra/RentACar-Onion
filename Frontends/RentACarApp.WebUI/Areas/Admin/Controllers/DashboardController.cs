e35c95cc11e944f8c1696cb9ba6ff8dd3f6b46d2
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-10 21:54:57 +0300

    Dashboard tasarımı yapılmaya başlandı.

---
+++ Frontends/RentACarApp.WebUI/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RentACarApp.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("/Admin/Dashboard")]
+    public class DashboardController : Controller
+    {
+        [Route("Index")]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
