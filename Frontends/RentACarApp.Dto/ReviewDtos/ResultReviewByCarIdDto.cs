02bda96d895217df15eb8b1e77ae0df29c911a8b
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-11 22:18:55 +0300

    değerlendirmeler çekildi.

---
+++ Frontends/RentACarApp.Dto/ReviewDtos/ResultReviewByCarIdDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarApp.Dto.ReviewDtos
+{
+    public class ResultReviewByCarIdDto
+    {
+        public int reviewID { get; set; }
+        public string name { get; set; }
+        public string image { get; set; }
+        public string comment { get; set; }
+        public int starCount { get; set; }
+        public DateTime date { get; set; }
+
+        public int carID { get; set; }
+    }
+}
---
+++ Frontends/RentACarApp.WebUI/ViewComponents/CarDetailComponents/_CarDetailTabPageCarReviewsComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RentACarApp.Dto.CarDtos;
+using RentACarApp.Dto.ReviewDtos;
+
+namespace RentACarApp.WebUI.ViewComponents.CarDetailComponents
+{
+    public class _CarDetailTabPageCarReviewsComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _CarDetailTabPageCarReviewsComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7066/api/Reviews?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = System.Text.Json.JsonSerializer.Deserialize<List<ResultReviewByCarIdDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
