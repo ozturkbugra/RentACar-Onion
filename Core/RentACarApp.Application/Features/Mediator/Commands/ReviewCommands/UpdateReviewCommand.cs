53c4379480ad4a528c11b4b4b66e849a3b8ba843
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-11 22:32:05 +0300

    Review güncelleme işlemleri için fluent validation yapıldı.

---
+++ Core/RentACarApp.Application/Features/Mediator/Commands/ReviewCommands/UpdateReviewCommand.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarApp.Application.Features.Mediator.Commands.ReviewCommands
+{
+    public class UpdateReviewCommand : IRequest
+    {
+        public int ReviewID { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Comment { get; set; }
+        public int StarCount { get; set; }
+        public DateTime Date { get; set; }
+        public int CarID { get; set; }
+    }
+}
---
+++ Core/RentACarApp.Application/Validators/ReviewValidators/UpdateReviewValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using RentACarApp.Application.Features.Mediator.Commands.ReviewCommands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarApp.Application.Validators.ReviewValidators
+{
+    public class UpdateReviewValidator : AbstractValidator<UpdateReviewCommand>
+    {
+        public UpdateReviewValidator() {
+
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Lütfen Adı Boş Geçmeyiniz !");
+            RuleFor(x => x.Name).MinimumLength(20).WithMessage("En az 20 karakter giriniz !");
+            RuleFor(x => x.StarCount).InclusiveBetween(1, 5).WithMessage("Yıldız değeri 1 ile 5 arasında olmalıdır!");
+
+        }
+    }
+}
