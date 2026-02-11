3d280ac0421dd841e17c779cd680369a9bfad602
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-11 21:24:15 +0300

    kod tekrarından kaçılarak Best Practice uygulandı.

--- Frontends/RentACarApp.WebUI/ViewComponents/CarDetailComponents/_CarDetailMainCarFeatureComponentPartial.cs
+++ Frontends/RentACarApp.WebUI/ViewComponents/CarDetailComponents/_CarDetailMainCarFeatureComponentPartial.cs
@@ -5,24 +5,9 @@ namespace RentACarApp.WebUI.ViewComponents.CarDetailComponents
 {
     public class _CarDetailMainCarFeatureComponentPartial : ViewComponent
     {
-        private readonly IHttpClientFactory _httpClientFactory;
-
-        public _CarDetailMainCarFeatureComponentPartial(IHttpClientFactory httpClientFactory)
+        public IViewComponentResult Invoke(ResultCarWithBrandDto values)
         {
-            _httpClientFactory = httpClientFactory;
-        }
-
-        public async Task<IViewComponentResult> InvokeAsync(int id)
-        {
-            var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7066/api/Cars/GetCarByIdWithBrand/" + id);
-            if (responseMessage.IsSuccessStatusCode)
-            {
-                var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = System.Text.Json.JsonSerializer.Deserialize<ResultCarWithBrandDto>(jsonData);
-                return View(values);
-            }
-            return View();
+            return View(values);
         }
     }
 }
