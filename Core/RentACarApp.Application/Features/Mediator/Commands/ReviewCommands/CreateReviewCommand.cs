5fdedbfd8c54ca0a5cecfaeaa137e905786c2c47
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-11 22:30:11 +0300

    Review ekleme işlemine fluent validation kontrolü eklendi.

---
+++ Core/RentACarApp.Application/Features/Mediator/Commands/ReviewCommands/CreateReviewCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarApp.Application.Features.Mediator.Commands.ReviewCommands
+{
+    public class CreateReviewCommand : IRequest
+    {
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Comment { get; set; }
+        public int StarCount { get; set; }
+        public DateTime Date { get; set; }
+        public int CarID { get; set; }
+    }
+}
