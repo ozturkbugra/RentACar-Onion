c1432d8a6408206e5b9f650c7fb7f31a85dd8d8e
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-12 19:11:41 +0300

    Kullanıcı kayıt işlemi uı'da yapıldı.

---
+++ Frontends/RentACarApp.WebUI/Controllers/RegisterController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentACarApp.Dto.AboutDtos;
+using RentACarApp.Dto.RegisterDtos;
+using System.Text;
+
+namespace RentACarApp.WebUI.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public RegisterController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateRegisterDto dto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(dto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7066/api/Registers", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            return View();
+        }
+    }
+}
