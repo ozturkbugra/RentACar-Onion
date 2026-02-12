bf4f4df5826a042093a83d52ad0314c30fdb7a23
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-12 08:33:31 +0300

    JwtTokenGenerator oluşturuldu.

---
+++ Core/RentACarApp.Application/Tools/JwtTokenGenerator.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using RentACarApp.Application.Dtos;
+using RentACarApp.Application.Features.Mediator.Results.AppUserResults;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarApp.Application.Tools
+{
+    public class JwtTokenGenerator
+    {
+        public static TokenResponseDto GenerateToken(GetCheckAppUserQueryResult result)
+        {
+            var claims = new List<Claim>();
+
+            if(!string.IsNullOrWhiteSpace(result.Role))
+                claims.Add(new Claim(ClaimTypes.Role, result.Role));
+
+            claims.Add(new Claim(ClaimTypes.NameIdentifier, result.Id.ToString()));
+
+            if (!string.IsNullOrWhiteSpace(result.UserName))
+                claims.Add(new Claim("UserName", result.UserName));
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(JwtTokenDefaults.Key));
+
+            var singInCredentials = new SigningCredentials(key,SecurityAlgorithms.HmacSha256);
+
+            var expireDate = DateTime.UtcNow.AddDays(JwtTokenDefaults.Expire);
+
+            JwtSecurityToken token = new JwtSecurityToken(issuer: JwtTokenDefaults.ValidIssuer, audience: JwtTokenDefaults.ValidAudiance, claims: claims, notBefore: DateTime.UtcNow, expires: expireDate, signingCredentials: singInCredentials);
+
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+
+            return new TokenResponseDto(handler.WriteToken(token), expireDate);
+        }
+    }
+}
