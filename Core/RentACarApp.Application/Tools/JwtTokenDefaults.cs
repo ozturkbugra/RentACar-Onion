1c66ff533b7417fab0763e0e860553afb90d4634
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-12 08:21:48 +0300

    JWT Response Dto'su ve Default JWT Token sınıfı eklendi.

---
+++ Core/RentACarApp.Application/Tools/JwtTokenDefaults.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarApp.Application.Tools
+{
+    public class JwtTokenDefaults
+    {
+        public const string ValidAudiance = "https://localhost";
+        public const string ValidIssuer = "https://localhost";
+        public const string Key = "RentACarProject";
+        public const int Expire = 3;
+    }
+}
