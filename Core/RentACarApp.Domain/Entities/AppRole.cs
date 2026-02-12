9b1aefc6c879be462c4790cccc3ca4a9b69823f4
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-12 07:47:25 +0300

    AppUser ve AppRole tabloları eklendi.

---
+++ Core/RentACarApp.Domain/Entities/AppRole.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarApp.Domain.Entities
+{
+    public class AppRole
+    {
+        public int AppRoleID { get; set; }
+
+        public string Name { get; set; }
+
+        public List<AppUser> AppUsers { get; set; }
+    }
+}
