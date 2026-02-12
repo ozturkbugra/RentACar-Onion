4002bb175ba49ff94b3842b6c77eb4834eb945bc
Buğra Öztürk <Bugra_ozturk1905@hotmail.com>
2026-02-12 20:28:40 +0300

    Login sayfasının tasarımı yapıldı.

---
+++ Frontends/RentACarApp.Dto/LoginDtos/CreateLoginDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarApp.Dto.LoginDtos
+{
+    public class CreateLoginDto
+    {
+        public string userName { get; set; }
+        public string password { get; set; }
+    }
+}
---
+++ Frontends/RentACarApp.WebUI/Controllers/LoginController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RentACarApp.WebUI.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string q)
+        {
+            return View();
+        }
+
+
+    }
+}
